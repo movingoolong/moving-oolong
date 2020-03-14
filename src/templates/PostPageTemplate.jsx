ef4b2f88435893f9be59d8228acc902d2826a6b1
Stephan Loh <sloh4137@gmail.com>
2020-03-14 17:11:59 -0400

    Add skeleton for a post page template

---
+++ src/templates/PostPageTemplate.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = {
+    root: {
+
+    }
+};
+
+function PostPageTemplate(props) {
+    return (
+        <>
+        </>
+    );
+}
+
+export default withStyles(styles)(PostPageTemplate)
