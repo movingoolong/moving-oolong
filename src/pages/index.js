d3229bc097f8e41b15022054da6b5ee69fc9f082
Stephan Loh <sloh4137@gmail.com>
2020-03-22 21:34:16 -0400

    Removed unnecessary react router

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,8 @@
 import React from "react";
-import {createMemoryHistory} from "history";
-import {Route, Router, Switch} from "react-router-dom";
 
 // pages for this product
 import HomePage from "./HomePage";
-import EpisodePage from "./EpisodePage";
-import AboutPage from "./AboutPage";
-
-
-let hist = createMemoryHistory();
 
 export default () => (
-  <Router history={hist}>
-    <Switch>
-      <Route path="/" component={HomePage} />
-      <Route path="/about" component={AboutPage} />
-      <Route path="/episodes" component={EpisodePage} />
-    </Switch>
-  </Router>
+  <HomePage />
 );
