f13bc45c775e288e01dfaaa79ef9757f37270ecc
Stephan Loh <sloh4137@gmail.com>
2020-04-18 16:35:34 -0400

    Add title to tabs

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -6,16 +6,40 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import NavBar from "./NavBar";
 import Footer from "./Footer";
 
+const NotCapitalizedWords = [
+    "the",
+    "a",
+    "or",
+];
+
 export default function Layout(props) {
     const { children, location } = props;
+    let locationString = location.pathname;
+    if(locationString == "/") {
+        // Just Home
+        locationString = "Home"
+    } else if (locationString.charAt(locationString.length - 1) == "/") {
+        // Remove beginning and trailing /
+        locationString = locationString.substring(1, locationString.length - 1);
+    } else {
+        // Just remove begining /
+        locationString = locationString.substring(1); 
+    }
+
+    // Go through and capitalize the first letter of each word if it's not in NotCapitalizedWords
+    const titleArray = locationString.split("-").map(word =>
+        (word.length > 1 && NotCapitalizedWords.indexOf(word) == -1) ?
+            word.charAt(0).toUpperCase() + word.substring(1)
+            : word);
+
     return (
         <>
-            <Helmet>
+            <Helmet title={`${titleArray.join(" ")} | Moving Oolong`} defer={false}>
                 <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet" />
-                <link href="https://fonts.googleapis.com/css?family=Passion+One&display=swap" rel="stylesheet"/>
+                <link href="https://fonts.googleapis.com/css?family=Passion+One&display=swap" rel="stylesheet" />
             </Helmet>
             <CssBaseline />
-            <NavBar location={location}/>
+            <NavBar location={location} />
             {children}
             <Footer />
         </>
