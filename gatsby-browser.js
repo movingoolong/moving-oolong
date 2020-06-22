8d69ac5d2f504ad2a09aaa6e38564e13fe090e9f
Stephan Loh <sloh4137@gmail.com>
2020-06-22 16:41:45 -0400

    Comment out gatsby-ssr and udupdate gatsby-browser to es6 syntax

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,21 +1,21 @@
-const React = require("react");
-const Layout = require("./src/components/Layout/Layout").default;
-const ThemeProvider = require("@material-ui/styles/ThemeProvider").default;
-const theme = require("./src/theme").default;
+import React from "react"
+import { ThemeProvider } from "@material-ui/core/styles"
+import Layout from "./src/components/Layout/Layout"
+import theme from "./src/theme"
 
-exports.wrapPageElement = ({ element, props }) => {
+export const wrapPageElement = ({ element, props }) => {
   // props provide same data to Layout as Page element will get
   // including location, data, etc - you don't need to pass it
   return <Layout {...props}>{element}</Layout>;
 };
 
-exports.wrapRootElement = ({ element }) => {
+export const wrapRootElement = ({ element }) => {
   return <ThemeProvider theme={theme}>{element}</ThemeProvider>;
 };
 
-exports.onClientEntry = () => {
+export const onClientEntry = () => {
   // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
   if (!(`IntersectionObserver` in window)) {
-    import(`intersection-observer`);
+      import(`intersection-observer`)
   }
-};
+}
