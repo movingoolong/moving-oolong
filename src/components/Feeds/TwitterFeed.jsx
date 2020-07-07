97830b5e810c61da20ab21e7e55b87368a17eaa9
Stephan Loh <sloh4137@gmail.com>
2020-07-07 19:34:30 -0400

    Add client only twitter feed

--- src/components/Feeds/TwitterFeed.jsx
+++ src/components/Feeds/TwitterFeed.jsx
@@ -1,16 +1,19 @@
 import React from "react"
+import ClientOnly from "components/General/ClientOnly"
 
 function TwitterFeed(props) {
     const { height } = props
     return (
-        <a
-            className="twitter-timeline"
-            data-theme="light"
-            data-height={height}
-            href="https://twitter.com/movingoolongpod?ref_src=twsrc%5Etfw"
-        >
-            Tweets by movingoolongpod
-        </a>
+        <ClientOnly>
+            <a
+                className="twitter-timeline"
+                data-theme="light"
+                data-height={height}
+                href="https://twitter.com/movingoolongpod?ref_src=twsrc%5Etfw"
+            >
+                Tweets by movingoolongpod
+            </a>
+        </ClientOnly>
     )
 }
 
