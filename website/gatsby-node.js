db14c7762c8bb917cc61ba9fa09c900fbb1da102
Stephan Loh <sloh4137@gmail.com>
2021-06-20 23:56:05 -0400

    Add webpack aliases from typescript

--- website/gatsby-node.js
+++ website/gatsby-node.js
@@ -1,7 +1,25 @@
 const path = require(`path`)
 const dayjs = require("dayjs")
-const siteConfig = require("./data/SiteConfig")
 const { createFilePath } = require(`gatsby-source-filesystem`)
+const siteConfig = require("./data/SiteConfig")
+const tsconfig = require("./tsconfig.json")
+
+const trimString = (str) => str.substring(0, str.length - 2)
+
+// Takes in paths defined in tsconfig.json, strips them of * and then adds them as webpack aliases
+exports.onCreateWebpackConfig = ({ actions }) => {
+    const aliases = tsconfig["compilerOptions"]["paths"]
+    const webpackAliases = {}
+    for (let key in aliases) {
+        const value = trimString(aliases[key][0])
+        webpackAliases[trimString(key)] = path.isAbsolute(value) ? value : path.resolve(value)
+    }
+    actions.setWebpackConfig({
+        resolve: {
+            alias: webpackAliases
+        }
+    })
+}
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
     const { createNodeField } = actions
