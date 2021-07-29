286d3dee695eefe6e6ef1a7c7a1275f14c4412e9
Stephan Loh <sloh4137@gmail.com>
2021-07-28 22:27:14 -0700

    Fix export for PropsForGrid

--- website/src/components/Layout/index.ts
+++ website/src/components/Layout/index.ts
@@ -2,4 +2,4 @@ export { default as AnimateOnVisible } from "./AnimateOnVisible"
 export { default as ContentGrid } from "./ContentGrid"
 
 // Types
-export { PropsForGrid } from "./ContentGrid"
+export type { PropsForGrid } from "./ContentGrid"
