00898649060a1f1f95fa29d6b18f292f216690ce
Stephan Loh <sloh4137@gmail.com>
2021-07-30 21:28:23 -0700

    Update sanity to use subtitle

--- sanitystudio/schemas/objects/portableText.ts
+++ sanitystudio/schemas/objects/portableText.ts
@@ -19,7 +19,7 @@ export default {
           { title: "H3", value: "h3" },
           { title: "H4", value: "h4" },
           { title: "H5", value: "h5" },
-          { title: "H6", value: "h6" },
+          { title: "Subtitle", value: "h6" },
           { title: "Quote", value: "blockquote" },
         ],
         lists: [
