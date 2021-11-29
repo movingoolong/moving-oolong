fb912d3b842f58c1631687f6876dda8273658179
Stephan Loh <sloh@fb.com>
2021-11-28 22:50:20 -0500

    Update bio

--- website/src/components/About/Bio.tsx
+++ website/src/components/About/Bio.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Card, CardContent, CardActions, makeStyles } from "@material-ui/core"
+import { Box, Card, CardContent, CardActions, makeStyles } from "@mui/material"
 import SocialIcons from "./SocialIcons"
 
 // Components
@@ -8,59 +8,47 @@ import { GatsbyImageIfExists } from "@components/Image"
 import SanityContent from "@components/SanityContent"
 
 // Types
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        height: "100%",
-        display: "flex",
-        flexDirection: "column",
-    },
-    content: {
-        height: "100%",
-        display: "flex",
-        flexDirection: "column",
-        flexShrink: 1,
-    },
-    title: {
-        fontWeight: 700,
-        marginTop: theme.spacing(1),
-        marginBottom: theme.spacing(1),
-    },
-    grow: {
-        flexGrow: 1,
-    },
-    socials: {
-        flexGrow: 1,
-        marginLeft: "auto",
-        marginRight: "auto",
-    },
-}))
-
 type Props = {
     bio: GatsbyTypes.BioFragment
 }
 
 function Bio({ bio }: Props) {
-    const classes = useStyles()
     const { name, instagram, twitter, propic, _rawDescription } = bio
 
     return (
-        <Card className={classes.root}>
-            <div className={classes.content}>
+        <Card sx={{
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+        }}>
+            <Box sx={{
+                        height: "100%",
+                        display: "flex",
+                        flexDirection: "column",
+                        flexShrink: 1,
+            }}>
                 <GatsbyImageIfExists imageAsset={propic} />
                 <CardContent>
                     <Text
                         variant="h6"
                         align="center"
-                        className={classes.title}
                         color="secondary"
+                        sx={{
+                            fontWeight: 700,
+                            marginTop: 1, // theme.spacing(1)
+                            marginBottom: 1, // theme.spacing(1)
+                        }}
                     >
                         {name}
                     </Text>
                     <SanityContent blocks={_rawDescription} />
                 </CardContent>
-            </div>
-            <CardActions disableSpacing className={classes.socials}>
+            </Box>
+            <CardActions disableSpacing sx={{
+                        flexGrow: 1,
+                        marginLeft: "auto",
+                        marginRight: "auto",
+            }}>
                 <SocialIcons instagram={instagram} twitter={twitter} />
             </CardActions>
         </Card>
