d69b4d38bcb765e7c348faa6cd96ee6c09597c94
Stephan Loh <sloh@fb.com>
2021-11-28 23:54:11 -0500

    Update image section

--- website/src/components/Image/ImageSection.tsx
+++ website/src/components/Image/ImageSection.tsx
@@ -1,23 +1,22 @@
 import React from "react"
 import { GatsbyImageProps } from "gatsby-plugin-image"
 import { BgImage } from "gbimage-bridge"
-import { makeStyles } from "@material-ui/core"
+import { styled } from "@mui/material"
 
 import { SanityImageWithAltText } from "./GatsbyImageIfExists"
 
-const useStyles = makeStyles((theme) => ({
-    imageFilter: {
-        height: "100%",
-        width: "100%",
-        position: "absolute",
-        top: 0,
-        background: "rgba(0, 0, 0, 0.4)",
-    },
-    content: {
-        width: "100%",
+const ImageFilter = styled("div")({
+    height: "100%",
+    width: "100%",
+    position: "absolute",
+    top: 0,
+    background: "rgba(0, 0, 0, 0.4)",
+})
+
+const Content = styled("div")(({theme})=>({
+    width: "100%",
         height: "100%",
         padding: theme.spacing(2),
-    },
 }))
 
 // Defined with GatsbyTypes.Maybe so that we know image and alt are required fields
@@ -27,16 +26,15 @@ type Props = Omit<GatsbyImageProps, "image" | "alt"> & {
 }
 
 const ImageSection = ({ imageAsset, children, ...rest }: Props) => {
-    const classes = useStyles()
     if (!imageAsset || !imageAsset?.asset?.gatsbyImageData) {
-        return <div className={classes.content}>{children}</div>
+        return <Content>{children}</Content>
     }
 
     const { gatsbyImageData, altText } = imageAsset?.asset
     return (
         <BgImage image={gatsbyImageData} alt={altText} {...rest}>
-            <div className={classes.imageFilter} />
-            <div className={classes.content}>{children}</div>
+            <ImageFilter />
+            <Content>{children}</Content>
         </BgImage>
     )
 }
